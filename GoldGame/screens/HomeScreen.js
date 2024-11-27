d8d38ab0b49bd36623bb42908bf7e7fc712b68a5
nktmoroz <nktmoroz@hotmail.com>
2024-11-27 08:09:51 -0800

    safety

--- GoldGame/screens/HomeScreen.js
+++ GoldGame/screens/HomeScreen.js
@@ -1,10 +1,44 @@
-import { View, Text, StyleSheet } from 'react-native';
-import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {authorize,refresh, revoke } from 'react-native-app-auth'
+import { TextInput } from 'react-native-web';
 
 export default function HomeScreen() {
+
+  const credentials ={
+    username: 'Nikita',
+    password: '1234'
+  }
+
+  const [access, setAccess] = useState(null);
+
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+
+  const login = async () => {
+    try{
+      const result = await authorize(credentials);
+      setAccess(result.access);
+    }catch (error){
+      console.log('Loging Error: ', error)
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Home Screen</Text>
+    <View>
+      {access ? (
+        <View>
+          <Text>Logged In!</Text>
+        </View>
+      ) : (
+        <View style={styles.container}>
+          <TextInput placeholder='Username' style={styles.credentials}></TextInput>
+          <TextInput placeholder='Password' style={styles.credentials} secureTextEntry={true}></TextInput>
+          <TouchableOpacity onPress={login} style={styles.button}>
+            <Text style={styles.buttonText}>Log In</Text>
+          </TouchableOpacity>
+        </View>
+      )}
     </View>
   );
 }
@@ -13,6 +47,28 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,                    
     justifyContent: 'center',   
-    alignItems: 'center',       
+    alignItems: 'center',
+    width: '100%',
+  },
+  credentials:{
+    alignItems: 'center',
+    width: '40%',
+    textAlign: 'center',
+    margin: 10,
+    borderWidth: 3,
+    borderRadius: 10,
+    fontSize: 20
+  },    
+  button: {
+    borderWidth: 3,
+    borderColor: 'black',
+    borderRadius: 10,
+    padding: 5,
+    marginTop: 10,
+  },
+  buttonText: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    justifyContent: 'center'
   },
 });
